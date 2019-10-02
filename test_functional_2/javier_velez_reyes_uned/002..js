31dc987541048b7e8a8943ba93b53014274bf7ba
Eduardo A.F <eacevedof@yahoo.es>
2019-10-02 21:49:54 +0200

    sec 06 9. Consultando las Noticias en base a la selección

---
+++ test_functional_2/javier_velez_reyes_uned/002..js
@@ -0,0 +1,27 @@
+/*
+//https://youtu.be/f4qQN6Mli-M?t=175
+*/
+
+const basket = [
+  {product:"oranges", type:"food",amount:2,price:15},
+  {product:"bleach",  type:"home",amount:2,price:15},
+  {product:"pears",   type:"food",amount:3,price:45},
+  {product:"apples",  type:"food",amount:3,price:25},
+  {product:"gloves",  type:"home",amount:1,price:10}
+]
+
+get_total =  strtype  =>
+    basket
+        .filter(objprod => objprod.type === strtype)
+        .reduce(
+          (ac,objprod) => ac + objprod.amount + objprod.price
+          ,0)
+
+const food = "food"
+const home = "home"
+
+const totfood = get_total(food)
+console.log("totfood",totfood)
+
+const tothome = get_total(home)
+console.log("tothome",tothome)
