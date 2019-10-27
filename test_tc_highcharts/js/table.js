4840f2f1725b5643ca498cae59177b6c836bd086
Eduardo A.F <eacevedof@yahoo.es>
2019-10-27 15:39:32 +0100

    se ve el tpl

--- test_tc_highcharts/js/table.js
+++ test_tc_highcharts/js/table.js
@@ -1,16 +1,22 @@
-alert("table.js")
-console.log(orion.tc_charts.provider)
+console.log("table.js")
 
 orion.tc_charts.table = function(){
   const strcontid = "container"
+  const strjqid = "#container"
 
+  const _get_str_columns = (objcols) => {
+    return ""
+  }
 
-  const _async_render = async(){
-    const objpromis = orion.tc_charts.provider.get_async_table()
-    
+  const _async_render = async () => {
+    const objpromis = await orion.tc_charts.provider.get_async_table()
+    const strhtml = `
+    <h1>Hola Table</h1>
+    `
+    $(strjqid).html(strhtml)
   }
 
   return {
-    async_render: _render
+    async_render: _async_render
   }
 }()
