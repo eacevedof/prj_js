f497a10142928bd4999e48d7491f7afc328e9a04
Eduardo A.F <eacevedof@yahoo.es>
2019-10-27 18:29:36 +0100

    no me carga los totales en la columna final

--- test_tc_highcharts/js/table.js
+++ test_tc_highcharts/js/table.js
@@ -18,36 +18,17 @@ orion.tc_charts.table = function(){
   })
 
   const _add_rows = (objson) => {
-  
-    console.log("objson.data",objson.data,"objson.data typeof",typeof objson.data)
-    // objson.data.foreach((row, i) => {
-    //   //jQuery("#tbl_jqgrid").jqGrid('addRowData',i+1,row); 
-    // })
-
-    const mydata = [
-      {id:"1",legend:"2007-10-01","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-      {id:"2",legend:"2007-10-02","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-      {id:"3",legend:"2007-09-01","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
-      {id:"4",legend:"2007-10-04","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-      {id:"5",legend:"2007-10-05","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-      {id:"6",legend:"2007-09-06","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
-      {id:"7",legend:"2007-10-04","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-      {id:"8",legend:"2007-10-03","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-      {id:"9",legend:"2007-09-01","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"}
-    ];
-
-    for(var i=0;i<=mydata.length;i++){
-    console.log("i",i,"mydata[i]:",mydata[i])
-      jQuery("#tbl_jqgrid").jqGrid('addRowData',i+1,mydata[i]);   
-    }
+    const rows = objson.data
+    rows.forEach( (row, i) => jQuery(strjqid).jqGrid('addRowData',i+1,row) )
   }
   
   const _load_jqgrid = (objson)=>{
+    
     const colnames = _get_colnames(objson)
     const colmodel = _get_colmodel(objson)
     
     //console.log("colmodel",colmodel)
-    jQuery("#tbl_jqgrid").jqGrid({
+    jQuery(strjqid).jqGrid({
       //http://www.trirand.com/blog/jqgrid/jqgrid.html?utm_source=weibolife
       datatype: "local",
       height: 250,
