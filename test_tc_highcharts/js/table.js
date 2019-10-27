1dbf5c4744d998f4650b33574754c0743671712f
Eduardo A.F <eacevedof@yahoo.es>
2019-10-27 18:10:00 +0100

    cambiando la carga de filas

--- test_tc_highcharts/js/table.js
+++ test_tc_highcharts/js/table.js
@@ -10,17 +10,36 @@ orion.tc_charts.table = function(){
 
   const _get_colmodel = objson => objson.cols.map((model, i) => {
     return {
-      name : model.title,
+      name : i===0 ? "legend":model.title,
       index: i,
       width: model.width,
       sorttype: model.fieldtype
     }
   })
+
+  const _add_rows = (objson) => {
+  
+
+    const mydata = [
+      {id:"1",legend:"2007-10-01","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
+      {id:"2",legend:"2007-10-02","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
+      {id:"3",legend:"2007-09-01","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
+      {id:"4",legend:"2007-10-04","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
+      {id:"5",legend:"2007-10-05","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
+      {id:"6",legend:"2007-09-06","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
+      {id:"7",legend:"2007-10-04","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
+      {id:"8",legend:"2007-10-03","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
+      {id:"9",legend:"2007-09-01","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"}
+      ];
+  for(var i=0;i<=mydata.length;i++)
+    jQuery("#tbl_jqgrid").jqGrid('addRowData',i+1,mydata[i]);   
+  }
   
   const _load_jqgrid = (objson)=>{
     const colnames = _get_colnames(objson)
     const colmodel = _get_colmodel(objson)
-    console.log("colnames:",colnames,"colmodel",colmodel)
+    
+    //console.log("colmodel",colmodel)
     jQuery("#tbl_jqgrid").jqGrid({
       //http://www.trirand.com/blog/jqgrid/jqgrid.html?utm_source=weibolife
       datatype: "local",
@@ -30,25 +49,15 @@ orion.tc_charts.table = function(){
          colModel: colmodel,
          multiselect: false,
          caption: "Evolution "
-    });
-    var mydata = [
-        {id:"1",Unsubs:"2007-10-01","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-        {id:"2",Unsubs:"2007-10-02","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-        {id:"3",Unsubs:"2007-09-01","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
-        {id:"4",Unsubs:"2007-10-04","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-        {id:"5",Unsubs:"2007-10-05","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-        {id:"6",Unsubs:"2007-09-06","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
-        {id:"7",Unsubs:"2007-10-04","Engineering_MPay  [10]":"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-        {id:"8",Unsubs:"2007-10-03","Engineering_MPay  [10]":"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-        {id:"9",Unsubs:"2007-09-01","Engineering_MPay  [10]":"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"}
-        ];
-    for(var i=0;i<=mydata.length;i++)
-      jQuery("#tbl_jqgrid").jqGrid('addRowData',i+1,mydata[i]);    
+    })
+
+    _add_rows(objson)
   }//_load_jqgrid
 
   const _async_render = async () => {
     const objson = await objprovider.get_async_table() //no va
-    console.log("_async_render.objson",objson)
+    console.log("_async_render.objson.report.cols",objson.report.cols)
+    console.log("_async_render.objson.report.data",objson.report.data)
     _load_jqgrid(objson.report)
   }
 
