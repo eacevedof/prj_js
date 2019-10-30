b56f8088aeafdf5ca37432898e3efccb4a3c5397
Eduardo A.F <eacevedof@yahoo.es>
2019-10-30 22:29:50 +0100

    notas

--- test_rxjs/lesson.js
+++ test_rxjs/lesson.js
@@ -6,9 +6,13 @@ function print(val){
   document.body.appendChild(el)
 }
 
+//se crea un observable cualquiera al que se le configura
+//una función observer desconocida a la que se le pasarán unos argumentos fijos
 const observable = Rx.Observable.create(observer => {
   observer.next("hello")
   observer.next("world")  
 })
 
+//despues de configurar el boservador se le suscribe la función
+//que llamará el observador
 observable.subscribe(val => print(val))
