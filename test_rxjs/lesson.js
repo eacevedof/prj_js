82b2f05a60c42025732c4131b4fd4461a2d5c1d8
Eduardo A.F <eacevedof@yahoo.es>
2019-10-30 22:04:57 +0100

    ejemplo 1 ya funca

--- test_rxjs/lesson.js
+++ test_rxjs/lesson.js
@@ -1,7 +1,14 @@
 console.log("lesson.js")
 
 function print(val){
-    let el = document.createElement("p")
-    el.innerText = val
-    document.body.appendChild(el)
+  let el = document.createElement("p")
+  el.innerText = val
+  document.body.appendChild(el)
 }
+
+const observable = Rx.Observable.create(observer => {
+  observer.next("hello")
+  observer.next("world")  
+})
+
+observable.subscribe(val => print(val))
