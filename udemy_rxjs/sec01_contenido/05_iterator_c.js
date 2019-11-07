c1b0e930b2422ec6e5149046e8c6e65afd268618
Eduardo A.F <eacevedof@yahoo.es>
2019-11-07 21:24:35 +0100

    05_iterator_c 2 obs escuchando un stream

---
+++ udemy_rxjs/sec01_contenido/05_iterator_c.js
@@ -0,0 +1,35 @@
+//node 05_iterator_c.js
+
+const { interval } = require("rxjs");
+const { map } = require("rxjs/operators")
+
+
+const observrnd = interval(1000).pipe(map(n => {console.log("n:",n);return Math.random()}))
+
+const fn_observer1 = n => console.log("obs1: ",n)
+const fn_observer2 = n => console.log("obs2: ",n) 
+
+//aunque parezca que dos observers están escuchando al mismo flujo no es así, el flujo 
+//esta duplicado. Esto se da pq hay una subscripcion por cada observer, es decir una ejecución 
+//por cada observable.
+const subs1 = observrnd.subscribe(fn_observer1)
+const subs2 = observrnd.subscribe(fn_observer2)
+
+setTimeout(() => {subs1.unsubscribe();subs2.unsubscribe()},4000)
+
+/*
+MINGW64 /e/projects/prj_js/udemy_rxjs/sec01_contenido (master)
+$ node 05_iterator_c.js
+n: 0
+obs1:  0.9002402719047111
+n: 0
+obs2:  0.12215605677229258
+n: 1
+obs1:  0.6289247678345251
+n: 1
+obs2:  0.7044494248227096
+n: 2
+obs1:  0.6457607264453746
+n: 2
+obs2:  0.6556825122189047
+*/
