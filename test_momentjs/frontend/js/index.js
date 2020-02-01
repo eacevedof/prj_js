d506d20d5dbae18779d58a6fd984eef8a36d6a57
ioedumac <eacevedof@yahoo.es>
2020-02-01 18:02:51 +0100

    no me va el remove del tbody

--- test_momentjs/frontend/js/index.js
+++ test_momentjs/frontend/js/index.js
@@ -1,9 +1,11 @@
 //index.js
 lg("index.js")
 import configbutton from "./modules/button.js"
+import configform from "./modules/form.js"
 
 const on_docready = () => {
   configbutton()
+  configform()
 }
 
 document.addEventListener("DOMContentLoaded", on_docready);
---
+++ test_momentjs/frontend/js/modules/form.js
@@ -0,0 +1,17 @@
+lg("form.js")
+
+const configform = ()=>{
+  const objnow = new Date();
+  const ardate = [
+    objnow.getFullYear(),objnow.getMonth(),objnow.getDate() 
+  ]
+  const strtoday = ardate.join("/")
+  lg("strtoday:",strtoday)
+
+  //$("#fecha_inicio").val(strtoday)
+  //$("#fecha_fin").val(strtoday)
+  $("#fecha_inicio")[0].valueAsDate = new Date()
+  $("#fecha_fin")[0].valueAsDate = new Date()
+}
+
+export default configform
