8476db65060349c5c21add74d7e64b9bd9a5ec31
ioedumac <eacevedof@yahoo.es>
2020-02-01 12:36:40 +0100

    logs

--- test_momentjs/backphp/app/phpdate.php
+++ test_momentjs/backphp/app/phpdate.php
@@ -5,13 +5,48 @@ use App\Appbase;
 
 final class Phpdate extends Appbase
 {
+    
     public function __construct()
     {
         parent::__construct();
+        $this->request = $this->_get_request();
+    }
+
+    private function _get_request()
+    {
+        $opearion = $this->get_post("operation");
+        $fechaini = $this->get_post("fecha_inicio");
+        $fechafin = $this->get_post("fecha_fin");
+        $interval = $this->get_post("interval");
+        $parts = explode(" ",$interval);
+        $i = $parts[0];
+        $period = $parts[1];
+
+        return [
+            "fechaini" => $fechaini,
+            "fechafin" => $fechafin,
+            "interval" => $interval,
+            "i" => $fechaini,
+            "period" => $period
+        ];
+    }
+
+    private function add_interval($fecha)
+    {
+        $strtotime = strtotime(date($fecha)."+ ".$this->request["interval"]);
+        return date("Ymd",$strtotime);
+    }
+
+    private function subtract_interval($fecha)
+    {
+        $strtotime = strtotime(date($fecha)."- ".$this->request["interval"]);
+        return date("Ymd",$strtotime);
     }
 
     public function index()
     {
 
+
     }
-}
+
+}//Phpdate
