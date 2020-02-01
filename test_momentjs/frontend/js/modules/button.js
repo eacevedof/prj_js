79f334b50eaf786151604a6cb16aab4a31020259
ioedumac <eacevedof@yahoo.es>
2020-02-01 19:31:54 +0100

    agrega y borra fechas

--- test_momentjs/frontend/js/modules/button.js
+++ test_momentjs/frontend/js/modules/button.js
@@ -3,13 +3,18 @@ import get_fechasphp from "./fechasphp.js"
 import render from "./table.js"
 
 const btnid = "btn-send"
-const ebutton = document.getElementById(btnid)
+const esend = document.getElementById(btnid)
+
+const eclear = document.getElementById("btn-cleartable")
 
 const configbutton = () => {
-    ebutton.addEventListener("click", async ()=>{
+    esend.addEventListener("click", async ()=>{
         const fechaphp = await get_fechasphp()
         lg("fechaphp resp",fechaphp)
         render(fechaphp)
+    })
+    eclear.addEventListener("click",()=>{
+        $("tr[id]").remove()
     })
 }
 
