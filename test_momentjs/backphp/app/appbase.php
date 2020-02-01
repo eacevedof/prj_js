c449465f36c6369f700e3388982f4ea1079c62ec
ioedumac <eacevedof@yahoo.es>
2020-02-01 12:10:35 +0100

    mini back creado

---
+++ test_momentjs/backphp/app/appbase.php
@@ -0,0 +1,27 @@
+<?php
+namespace App;
+include_once "json.php";
+use App\Json;
+
+abstract class Appbase
+{
+    protected $post = [];
+    protected $get = [];
+    protected $response = [];
+
+    public function __construct()
+    {
+        $this->post = $_POST;
+        $this->get = $_GET;
+    }
+
+    protected function get_get($key=""){ return $key ? $this->get[$key] ?? null: $this->get; }
+    protected function get_post($key=""){ return $key ? $this->post[$key] ?? null: $this->post; }
+
+    public function index()
+    {
+        return (new Json())->toString();
+    }
+
+    public function get_response(){return $response;}
+}
