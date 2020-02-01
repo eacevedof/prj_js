0473dc6740b44a1f9dc325857ccb423e3180a1bd
ioedumac <eacevedof@yahoo.es>
2020-02-01 18:44:24 +0100

    ya calcula moment

--- test_momentjs/frontend/js/modules/fechasmoment.js
+++ test_momentjs/frontend/js/modules/fechasmoment.js
@@ -1,10 +1,43 @@
 lg("fechasmoment.js")
 
-const get_fechamoment = ()=>{
-  return {
-    fechaini1: "",
-    fechafin1: ""
+const get_formdata = ()=>(
+  {
+    fechaini:   $("#fecha_inicio").val(),
+    fechafin:   $("#fecha_fin").val(),
+    opearation: $("#sel-operation").val(),
+    i:          $("#num-units").val(),
+    period:     $("#sel-period").val(),
   }
+)
+
+const get_added = (fecha,frmdata) => {
+  
+  const newdate = moment(fecha).add(frmdata.i,frmdata.period).format("YYYYMMDD")
+  lg("newdate",newdate)
+  return newdate
+}
+
+
+const get_subtracted = (fecha,frmdata) => {
+  
+  const newdate = moment(fecha).subtract(frmdata.i,frmdata.period).format("YYYYMMDD")
+  lg("newdate",newdate)
+  return newdate
+}
+
+
+const get_fechamoment = ()=>{
+  const frmdata = get_formdata()
+
+  if(frmdata.opearation=="add")
+    return {
+      fechaini1: get_added(frmdata.fechaini,frmdata),
+      fechafin1: get_added(frmdata.fechafin,frmdata)
+    }
+  return {
+    fechaini1: get_subtracted(frmdata.fechaini,frmdata),
+    fechafin1: get_subtracted(frmdata.fechafin,frmdata)
+  }  
 }
 
 export default get_fechamoment
