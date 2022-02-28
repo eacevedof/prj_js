dd2fe9d57622052dfeca0629895fd141b7a04a88
ioedumac <eacevedof@yahoo.es>
2022-02-28 23:02:43 +0100

    lit-3

---
+++ test_lit_local/lit.dev-v3/lit/reactive-element/decorators/state.js
@@ -0,0 +1,11 @@
+import {property as e} from "./property.js";
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+function t(t2) {
+  return e({...t2, state: true});
+}
+export {t as state};
+export default null;
