2f966a1ac73048a38d51273510075c0eeaadeebe
ioedumac <eacevedof@yahoo.es>
2022-08-12 15:10:58 +0200

    qr-capture class php

--- test-read-qr/qr-image.php
+++ test-read-qr/qr-image.php
@@ -8,8 +8,8 @@ $pathimages = dirname(__FILE__)."/images";
 $now = date("Y-m-d_H-i-s");
 $filename = "$pathimages/generated-$now.png";
 
-
-QRcode::png("codigo-qr-1234", $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+$value = $_GET["to-qr"] ?? ":)";
+QRcode::png($value, $filename, $errorCorrectionLevel, $matrixPointSize, 2);
 
 $img = basename($filename);
 $img = "./images/$img";
