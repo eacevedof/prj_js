b4dc5c68187a2afeae5e049f211ff825542893c0
ioedumac <eacevedof@yahoo.es>
2022-08-12 12:29:06 +0200

    qr-capture

--- test-read-qr/index.js
+++ test-read-qr/index.js
@@ -3,46 +3,37 @@ if (!("BarcodeDetector" in window)) {
   console.log("not compatible")
 }
 
-const video = document.querySelector("#video")
-// Check if device has camera
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Use video without audio
-  const constraints = {
-    video: true,
-    audio: false
+let $video = null
+let codedetector = null
+
+const $btn = document.getElementById("btn-capture")
+$btn.addEventListener("click", function (){
+  $video = document.querySelector("#video")
+  // Check if device has camera
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Use video without audio
+    const constraints = {
+      video: true,
+      audio: false
+    }
+
+    // Start video stream
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => $video.srcObject = stream);
   }
-
-  // Start video stream
-  navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream);
-}
-
-const barcodeDetector = new BarcodeDetector({ formats: ["qr_code"] });
-
-/*
-let formats;
-// Save all formats to formats var
-BarcodeDetector.getSupportedFormats().then(arr => formats = arr);
-// Create new barcode detector with all supported formats
-const barcodeDetector = new BarcodeDetector({ formats });
-
-// Create new barcode detector
-const barcodeDetector = new BarcodeDetector({ formats: ["qr_code"] });
-*/
-// Detect code function
-const detectCode = () => {
-  // Start detecting codes on to the video element
-  barcodeDetector.detect(video).then(codes => {
+  codedetector = new BarcodeDetector({ formats: ["qr_code"] })
+  codedetector.detect(video).then(codes => {
     // If no codes exit function
     if (codes.length === 0) return;
 
-    for (const barcode of codes)  {
+    for (const objcode of codes)  {
       // Log the barcode to the console
-      console.log(barcode);
+      console.log("objcode", objcode)
+      document.getElementById("qr-value").value = objcode.rawValue
+      clearInterval(idInterval)
     }
   }).catch(err => {
     // Log an error if one happens
     console.error(err);
   })
-}
 
-setInterval(detectCode, 100);
+})
