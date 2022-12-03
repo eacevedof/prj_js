efe076ef9fd2e38e28e9287a01c405fe97b7cb0d
ioedumac <eacevedof@yahoo.es>
2022-12-03 17:54:43 +0100

    advent.js reto errro tests

--- adventofcode/2022-12-01/01-calorie-counting.ts
+++ adventofcode/2022-12-01/01-calorie-counting.ts
@@ -1,1 +1,11 @@
 // https://adventofcode.com/2022/day/1
+/*
+reindeer (reindir): reno
+fruit (frut)
+grove (groof): arboleda
+energy (enershi)
+although (aldou)
+Elves (elfs)
+available (aveilibel)
+separates (separeits)
+*/
