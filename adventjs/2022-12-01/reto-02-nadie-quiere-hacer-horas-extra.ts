8bff80253f691f03bb6b4fe68fc3ef476c0b87d6
ioedumac <eacevedof@yahoo.es>
2022-12-03 17:06:26 +0100

    advent.js reto errro tests

--- adventjs/2022-12-01/reto-02-nadie-quiere-hacer-horas-extra.ts
+++ adventjs/2022-12-01/reto-02-nadie-quiere-hacer-horas-extra.ts
@@ -1,15 +1,15 @@
 //https://adventjs.dev/es/challenges/2022/2
 //reto-02-nadie-quiere-hacer-horas-extra.ts
-const year: string = "2022"
-const holidays: Array<string> = ["01/06", "04/01", "12/25"] // formato MM/DD
+const YEAR: number = 2022
+const HOLLYDAYS: Array<string> = ["01/06", "04/01", "12/25"]
 
-const countHours = (year: string, holidays:Array<string>): number => { 
+const countHours = (year: number, holidays:Array<string>): number => { 
   const EXTRA_HOURS: number = 2
   const SATURDAY: number = 6
   const SUNDAY: number = 0
 
   const countableDays: Array<boolean> = holidays.map((day:string) => {
-    const yyymmdd: string = year.concat(day)
+    const yyymmdd: string = year.toString().concat("/").concat(day)
     const daypos: number = (new Date(yyymmdd)).getDay()
     return ![SATURDAY, SUNDAY].includes(daypos)
   })
@@ -17,5 +17,5 @@ const countHours = (year: string, holidays:Array<string>): number => {
   return countableDays.filter((isHoliday:boolean) => isHoliday).length * EXTRA_HOURS
 }
 
-const hours = countHours(year, holidays) // 2 días -> 4 horas extra en el año
+const hours = countHours(YEAR, HOLLYDAYS)
 console.log(hours)
