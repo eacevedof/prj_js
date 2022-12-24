fb8b0db0e4aa7b4039af2f2ab673961a32caa7a8
ioedumac <eacevedof@yahoo.es>
2022-12-24 07:56:36 +0100

    _SERVER

--- php-server/index.php
+++ php-server/index.php
@@ -1,3 +1,13 @@
 <?php
 echo "<pre>";
+$server = $_SERVER;
+unset(
+    $server["REDIRECT_DOCUMENT_ROOT"],
+    $server["SERVER_ADDR"],
+    $server["SERVER_SOFTWARE"],
+    $server["DOCUMENT_ROOT"],
+    $server["CONTEXT_DOCUMENT_ROOT"],
+    $server["SCRIPT_FILENAME"],
+    $server["ORIG_PATH_TRANSLATED"],
+);
 print_r($_SERVER);
