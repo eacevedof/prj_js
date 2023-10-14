02ee9a8f1de989ecfd60999c6c3f97f6b3af1fec
ioedumac <eacevedof@yahoo.es>
2023-10-14 17:07:27 +0200

    backup ejercicio

--- midudev/testchallenge-node/excercise-1.js
+++ midudev/testchallenge-node/excercise-1.js
@@ -1,21 +1,27 @@
 //Arregla esta función para que el código posterior funcione como se espera:
 import net from 'node:net'
 
-export const ping = (ip) => {
+export const ping = (ip, fnCallback) => {
   const startTime = process.hrtime()
 
-  const client = net.connect({ port: 80, host: ip }, () => {
-    client.end()
-    return { time: process.hrtime(startTime), ip }
+  const client = net.connect(
+    { host: ip, port: 80},
+    () => {
+      client.end()
+      return { time: process.hrtime(startTime), ip }
   })
 
   client.on('error', (err) => {
-    throw err
     client.end()
+    throw err
   })
 }
 
-ping('midu.dev', (err, info) => {
-  if (err) console.error(err)
+ping('eduardoaf.com', (err, info) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
   console.log(info)
 })
